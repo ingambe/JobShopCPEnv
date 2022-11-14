36a41b081a6bbd9244dccdb832b446a6d6bc1324
Pierre Tassel <pierre.tassel@outlook.fr>
2022-11-14 15:04:07 +0100

    fix test

--- tests/test_correct_solution.py
+++ tests/test_correct_solution.py
@@ -6,56 +6,53 @@ from tests.cp_checker import checkerSat
 
 
 def test_ta01():
-    if __name__ == '__main__':
-        env = CompiledJssEnvCP('../src/compiled_jss/instances/ta01')
-        obs = env.reset()
-        done = False
-        info = {}
-        while not done:
-            # sample without replacement from obs['action_mask'] boolean vector
-            action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
-                                      p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
-            obs, reward, done, info = env.step(action)
-        assert "makespan" in info, f'finished without makespan'
-        makespan_agent = int(info["makespan"])
-        solution_agent = json.loads(info["solution"])
-        correct_solution, cp_makespan = checkerSat(solution_agent, '../src/compiled_jss/instances/ta01')
-        assert correct_solution, f'agent solution is not correct'
-        assert makespan_agent == cp_makespan, f'makespan computation is not correct'
+    env = CompiledJssEnvCP('../src/compiled_jss/instances/ta01')
+    obs = env.reset()
+    done = False
+    info = {}
+    while not done:
+        # sample without replacement from obs['action_mask'] boolean vector
+        action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
+                                  p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
+        obs, reward, done, info = env.step(action)
+    assert "makespan" in info, f'finished without makespan'
+    makespan_agent = int(info["makespan"])
+    solution_agent = json.loads(info["solution"])
+    correct_solution, cp_makespan = checkerSat(solution_agent, '../src/compiled_jss/instances/ta01')
+    assert correct_solution, f'agent solution is not correct'
+    assert makespan_agent == cp_makespan, f'makespan computation is not correct'
 
 
 def test_ta80():
-    if __name__ == '__main__':
-        env = CompiledJssEnvCP('../src/compiled_jss/instances/ta80')
-        obs = env.reset()
-        done = False
-        info = {}
-        while not done:
-            # sample without replacement from obs['action_mask'] boolean vector
-            action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
-                                      p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
-            obs, reward, done, info = env.step(action)
-        assert "makespan" in info, f'finished without makespan'
-        makespan_agent = int(info["makespan"])
-        solution_agent = json.loads(info["solution"])
-        correct_solution, cp_makespan = checkerSat(solution_agent, '../src/compiled_jss/instances/ta80')
-        assert correct_solution, f'agent solution is not correct'
-        assert makespan_agent == cp_makespan, f'makespan computation is not correct'
+    env = CompiledJssEnvCP('../src/compiled_jss/instances/ta80')
+    obs = env.reset()
+    done = False
+    info = {}
+    while not done:
+        # sample without replacement from obs['action_mask'] boolean vector
+        action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
+                                  p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
+        obs, reward, done, info = env.step(action)
+    assert "makespan" in info, f'finished without makespan'
+    makespan_agent = int(info["makespan"])
+    solution_agent = json.loads(info["solution"])
+    correct_solution, cp_makespan = checkerSat(solution_agent, '../src/compiled_jss/instances/ta80')
+    assert correct_solution, f'agent solution is not correct'
+    assert makespan_agent == cp_makespan, f'makespan computation is not correct'
 
 def test_dmu01():
-    if __name__ == '__main__':
-        env = CompiledJssEnvCP('../src/compiled_jss/instances/dmu01.txt')
-        obs = env.reset()
-        done = False
-        info = {}
-        while not done:
-            # sample without replacement from obs['action_mask'] boolean vector
-            action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
-                                      p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
-            obs, reward, done, info = env.step(action)
-        assert "makespan" in info, f'finished without makespan'
-        makespan_agent = int(info["makespan"])
-        solution_agent = json.loads(info["solution"])
-        correct_solution, cp_makespan = checkerSat(solution_agent, '../src/compiled_jss/instances/dmu01.txt')
-        assert correct_solution, f'agent solution is not correct'
-        assert makespan_agent == cp_makespan, f'makespan computation is not correct'
+    env = CompiledJssEnvCP('../src/compiled_jss/instances/dmu01.txt')
+    obs = env.reset()
+    done = False
+    info = {}
+    while not done:
+        # sample without replacement from obs['action_mask'] boolean vector
+        action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
+                                  p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
+        obs, reward, done, info = env.step(action)
+    assert "makespan" in info, f'finished without makespan'
+    makespan_agent = int(info["makespan"])
+    solution_agent = json.loads(info["solution"])
+    correct_solution, cp_makespan = checkerSat(solution_agent, '../src/compiled_jss/instances/dmu01.txt')
+    assert correct_solution, f'agent solution is not correct'
+    assert makespan_agent == cp_makespan, f'makespan computation is not correct'
--- tests/test_flow_shop_scheduling.py
+++ tests/test_flow_shop_scheduling.py
@@ -9,57 +9,56 @@ from hypothesis.strategies import integers
 
 
 def test_tai_small():
-    if __name__ == '__main__':
-        env = CompiledJssEnvCP('../src/compiled_jss/instances/tai_j10_m10_1.data')
-        obs = env.reset()
-        done = False
-        info = {}
-        while not done:
-            # sample without replacement from obs['action_mask'] boolean vector
-            action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
-                                      p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
-            obs, reward, done, info = env.step(action)
-        assert "makespan" in info, f'finished without makespan'
-        makespan_agent = int(info["makespan"])
-        solution_agent = json.loads(info["solution"])
-        correct_solution, cp_makespan = checkerSat(solution_agent, '../src/compiled_jss/instances/tai_j10_m10_1.data')
-        assert correct_solution, f'agent solution is not correct'
-        assert makespan_agent == cp_makespan, f'makespan computation is not correct'
+    env = CompiledJssEnvCP('../src/compiled_jss/instances/tai_j10_m10_1.data')
+    obs = env.reset()
+    done = False
+    info = {}
+    while not done:
+        # sample without replacement from obs['action_mask'] boolean vector
+        action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
+                                  p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
+        obs, reward, done, info = env.step(action)
+    assert "makespan" in info, f'finished without makespan'
+    makespan_agent = int(info["makespan"])
+    solution_agent = json.loads(info["solution"])
+    correct_solution, cp_makespan = checkerSat(solution_agent, '../src/compiled_jss/instances/tai_j10_m10_1.data')
+    assert correct_solution, f'agent solution is not correct'
+    assert makespan_agent == cp_makespan, f'makespan computation is not correct'
+
 
 def test_tai_large():
-    if __name__ == '__main__':
-        env = CompiledJssEnvCP('../src/compiled_jss/instances/tai_j100_m100_1.data')
-        obs = env.reset()
-        done = False
-        info = {}
-        while not done:
-            # sample without replacement from obs['action_mask'] boolean vector
-            action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
-                                      p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
-            obs, reward, done, info = env.step(action)
-        assert "makespan" in info, f'finished without makespan'
-        makespan_agent = int(info["makespan"])
-        solution_agent = json.loads(info["solution"])
-        correct_solution, cp_makespan = checkerSat(solution_agent, '../src/compiled_jss/instances/tai_j100_m100_1.data')
-        assert correct_solution, f'agent solution is not correct'
-        assert makespan_agent == cp_makespan, f'makespan computation is not correct'
+    env = CompiledJssEnvCP('../src/compiled_jss/instances/tai_j100_m100_1.data')
+    obs = env.reset()
+    done = False
+    info = {}
+    while not done:
+        # sample without replacement from obs['action_mask'] boolean vector
+        action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
+                                  p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
+        obs, reward, done, info = env.step(action)
+    assert "makespan" in info, f'finished without makespan'
+    makespan_agent = int(info["makespan"])
+    solution_agent = json.loads(info["solution"])
+    correct_solution, cp_makespan = checkerSat(solution_agent, '../src/compiled_jss/instances/tai_j100_m100_1.data')
+    assert correct_solution, f'agent solution is not correct'
+    assert makespan_agent == cp_makespan, f'makespan computation is not correct'
 
 
 @given(integers(min_value=1, max_value=10))
 def test_tai_random_large(idx):
-    if __name__ == '__main__':
-        env = CompiledJssEnvCP(f'../src/compiled_jss/instances/tai_j100_m100_{idx}.data')
-        obs = env.reset()
-        done = False
-        info = {}
-        while not done:
-            # sample without replacement from obs['action_mask'] boolean vector
-            action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
-                                      p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
-            obs, reward, done, info = env.step(action)
-        assert "makespan" in info, f'finished without makespan'
-        makespan_agent = int(info["makespan"])
-        solution_agent = json.loads(info["solution"])
-        correct_solution, cp_makespan = checkerSat(solution_agent, f'../src/compiled_jss/instances/tai_j100_m100_{idx}.data')
-        assert correct_solution, f'agent solution is not correct'
-        assert makespan_agent == cp_makespan, f'makespan computation is not correct'
+    env = CompiledJssEnvCP(f'../src/compiled_jss/instances/tai_j100_m100_{idx}.data')
+    obs = env.reset()
+    done = False
+    info = {}
+    while not done:
+        # sample without replacement from obs['action_mask'] boolean vector
+        action = np.random.choice(np.arange(len(obs['action_mask'])), size=int(sum(obs['action_mask'])),
+                                  p=obs['action_mask'] / obs['action_mask'].sum(), replace=False)
+        obs, reward, done, info = env.step(action)
+    assert "makespan" in info, f'finished without makespan'
+    makespan_agent = int(info["makespan"])
+    solution_agent = json.loads(info["solution"])
+    correct_solution, cp_makespan = checkerSat(solution_agent,
+                                               f'../src/compiled_jss/instances/tai_j100_m100_{idx}.data')
+    assert correct_solution, f'agent solution is not correct'
+    assert makespan_agent == cp_makespan, f'makespan computation is not correct'
